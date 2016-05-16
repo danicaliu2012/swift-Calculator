8e5aed8f5b4de90ef7a28bfc3d25838a274cfffa
danicaliu2012 <danicaliu2012@gmail.com>
2016-05-15 17:24:34 -0700

    swift-calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
+
+
 
 class ViewController: UIViewController {
+    
+    
+    
+    var sound = NSURL(fileURLWithPath:
+        NSBundle.mainBundle().pathForResource("button", ofType: "mp3")!)
+    var audioPlayer = AVAudioPlayer()
 
+
+    @IBOutlet var btnResult: UITextField!
+    
+    var result = Float()
+    var currentNumber = Float()
+    
+    var currentOp = String()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: sound,
+                fileTypeHint: AVFileTypeMPEGLayer3)
+        } catch let error as NSError {
+            print("AV Sound Error: \(error.localizedDescription)")
+        }
+        
+        
+        
+        currentOp = "="
+        btnResult.text = ("\(result)")
+        
+       
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +52,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNumberInput(sender: UIButton) {
+        
+        currentNumber = currentNumber * 10 + Float(Int(sender.titleLabel!.text!)!)
+        btnResult.text = ("\(currentNumber)")
+        
+        audioPlayer.play()
+    }
 
+    @IBAction func btnOperation(sender: UIButton) {
+        switch currentOp{
+            case "=":
+            result = currentNumber
+            case "+":
+            result = result + currentNumber
+            case "-":
+            result = result - currentNumber
+            case "*":
+            result = result * currentNumber
+            case "/":
+            result = result / currentNumber
+            
+        default:
+            print("error")
+            
+        }
+        
+        currentNumber = 0
+        btnResult.text = ("\(result)")
+        if (sender.titleLabel!.text == "="){
+            result = 0
+        }
+        currentOp = sender.titleLabel!.text! as String!
+        
+        audioPlayer.play()
+    
+    }
+    
+    @IBAction func btnClear(sender: UIButton) {
+        result = 0
+        currentNumber = 0
+        currentOp = "="
+        btnResult.text = ("\(result)")
+        
+        audioPlayer.play()
+    
+    }
+    
+    
+    
 }
+
 
